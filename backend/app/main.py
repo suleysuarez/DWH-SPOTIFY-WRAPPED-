3f0ab326e6ff39e082361137f0e721e13204308c
JhonatanVeri <jhonatan.veraric@unipamplona.edu.co>
2026-05-14 15:51:02 -0500

    feat: ALLOW_HOSTS env variable for CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ app = FastAPI(
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL],
+    allow_origins=settings.get_allow_hosts(),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
